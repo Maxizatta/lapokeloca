c70aeb0cfa274ff5ce49239eb644248341a5a739
Maximiliano Zatta <maxizatta@gmail.com>
2023-01-27 00:04:47 -0300

    add gem collation, and take a lot of changes in show and index html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pokemons#index'
   get '/pokemons', to: 'pokemons#index'
   # get 'pokemons/:id', to: 'pokemons#show'
   get 'pokemons/:id', to: 'pokemons#show', as: 'pokemon'
